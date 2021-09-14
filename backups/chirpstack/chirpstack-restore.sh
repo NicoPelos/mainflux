d00cc45bf4f33edde4b4f3b31892300fdebd85fa
NicoPelos <pelichotti@gmail.com>
2021-09-14 20:49:51 -0300

    borrar chripstack

--- backups/chirpstack/chirpstack-restore.sh
+++
@@ -1,4 +0,0 @@
-echo comenzando restauración de volúmenes de chirpstack...
-
-echo restaurando chirpstack-docker_postgresqldata
-cat chirpstack-docker_postgresqldata.tar.bz2 | docker run -i -v chirpstack-docker_postgresqldata:/volume --rm loomchild/volume-backup restore -
